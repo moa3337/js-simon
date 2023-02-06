a735d9ef65c79a63adc02ed93795660618564374
moa3337 <moa-x@hotmail.com>
2023-02-06 14:28:30 +0100

    correzione final commit

--- js/main.js
+++ js/main.js
@@ -10,6 +10,36 @@ const hoursElement = document.getElementById("hours");
 const minutsElement = document.getElementById("minutes");
 const seconsElement = document.getElementById("seconds");
 
+// INIZIO DELLA "function" PER IL COUNTDOWN
+setInterval(function () {
+    // DICHIARAZIONE DELLA DATA E ORA ATTUALI
+    const now = new Date();
+    // DICHIARAZIONE DELLA DATA DEL COUNTDOWN
+    const tomorrowLesson = new Date("2023-02-08 09:30");
+
+    const msNow = now.getTime();
+    const msTomorrow = tomorrowLesson.getTime();
+    const msToTomorrow = msTomorrow - msNow;
+    const sToTomorrow = parseInt(msToTomorrow / 1000);
+
+    // CALCOLO DIFFERENZA TRA ORA ATTUALE E ORA DELLA SCADENZA
+    const seconds = sToTomorrow % 60;
+    const minutes = parseInt(sToTomorrow / 60) % 60;
+    const hours = parseInt(sToTomorrow / 60 / 60) % 24;
+    const days = parseInt(sToTomorrow / 60 / 60 / 24);
+
+    // STAMPO NELLA PAGINA IL COUNTDOWN AGGIUNGENDO LO "0" QUANDO IL NUMERO E' < 10
+    seconsElement.innerHTML = (seconds < 10) ? '0' + seconds : seconds;
+    minutsElement.innerHTML = (minutes < 10) ? '0' + minutes : minutes;
+    hoursElement.innerHTML = (hours < 10) ? '0' + hours : hours;
+    daysElement.innerHTML = (days < 10) ? '0' + days : days;
+
+    // FUNZIONE PER TENERE IL COUNTDOWN AGGIORNATO
+}, 1000);
+
+// METODO ALTERNATIVO:
+
+/*
 // DICHIARO LA DATA E L'ORA DEL TERMINE DEL COUNTDOWN
 const endTime = new Date();
 endTime.setDate(endTime.getDate() + 1);
@@ -24,17 +54,17 @@ function countDown() {
     const difference = endTime - now;
 
     // CALCOLO DIFFERENZA TRA ORA ATTUALE E ORA DELLA SCADENZA
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));     
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 *60));
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    
+
     // COLLEGAMENTO DEI TEMPI IMPOSTATI AGLI "id" HTML
     daysElement.innerHTML = days;
     hoursElement.innerHTML = hours;
     minutsElement.innerHTML = minutes;
-    seconsElement.innerHTML = seconds;   
+    seconsElement.innerHTML = seconds;
 }
-
 // FUNZIONE PER TENERE IL COUNTDOWN AGGIORNATO
 setInterval(countDown, 1000);
+*/
